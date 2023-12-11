ab5c214dbe20ae04d793a09ff70475ccd7ca90eb
517pacifikal <1444075503@qq.com>
2023-12-11 21:12:16 +0800

    Initial commit

---
+++ src/main/java/com/aaronwu/bler/BlerApplication.java
@@ -0,0 +1,13 @@
+package com.aaronwu.bler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aaronwu/bler/BlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaronwu.bler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
